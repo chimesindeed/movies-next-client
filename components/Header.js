336068bcfeee038ffb6f284819644c25b997c1bb
chimesindeed <gratitudeadore@fastmail.com>
2022-02-02 14:28:42 -0500

    installed icons library

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,20 @@
 import Image from 'next/image'
+import {
+  BadgeCheckIcon,
+  CollectionIcon,
+  HomeIcon,
+  LightningBoltIcon,
+  SearchIcon,
+  UserIcon,
+} from '@heroicons/react/outline'
+import HeaderItem from './HeaderItem'
+
 function Header(){
   return (
     <header className=''>
-      <h1>Header</h1>
+      <div>
+        <HeaderItem title="HOME" Icon={HomeIcon}/>
+      </div>
       <Image
         className='object-contain'
         src='https://links.papareact.com/ua6'
---
+++ components/HeaderItem.js
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+
+function HeaderItem({Icon, title}){
+  return (
+    <div>
+      <Icon className='h-8 mb-1' />
+      <p className= 'tracking-widest'>{title}</p>
+
+    </div>
+
+  )
+}
+
+export default HeaderItem
