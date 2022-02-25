be6df6af028dd797f321294f7a38c23aefddd991
chimesindeed <gratitudeadore@fastmail.com>
2022-02-25 05:42:43 -0500

    installed react-flip-move library; created forwardRef to Thumbnail component to be accessed by parent

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,11 +1,13 @@
 import Image from 'next/image'
+import { forwardRef } from 'react'
 import { ThumbUpIcon } from '@heroicons/react/outline'
 
-
-function Thumbnail({result}){
+const Thumbnail = forwardRef(({result}, ref) => {
   const BASE_URL = 'https://image.tmdb.org/t/p/original/'
   return (
-    <div className='p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50'>
+    <div
+      ref={ref}
+      className='p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50'>
       <Image
         layout='responsive'
         height={1080}
@@ -28,6 +30,6 @@ function Thumbnail({result}){
     </div>
   )
 
-}
+})
 
 export default Thumbnail
